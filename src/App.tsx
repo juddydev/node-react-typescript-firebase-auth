3f393302cc3f6dbfe0f5127d3655856300efbb9d
kwn <kwn1125dev@gmail.com>
2023-04-09 17:05:17 +0900

    feat: Update button style and consolidate theme provider
    - Add settings for button text transform and font weight in the theme's typography object.
    - Consolidate the theme provider to wrap the entire application, rather than wrapping each component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import { AuthProvider } from "./AuthContext";
 import { Home } from "./Home";
 import { SignIn } from "./SignIn";
 import { SignUp } from "./SignUp";
 
+const theme = createTheme({
+  typography: {
+    button: {
+      textTransform: "none",
+      fontWeight: "bold",
+    },
+  },
+});
+
 export const App = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </AuthProvider>
   );
 };
